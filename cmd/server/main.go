c3731c7379bfe0f98f6f85632fb9b5f8e8a40dde
nictes1 <nikolastesone@gmail.com>
2021-09-23 14:26:03 -0300

    add basicAuth with gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nictes1/API-Rest-Golang/controller"
+	"github.com/nictes1/API-Rest-Golang/middlewares"
 	"github.com/nictes1/API-Rest-Golang/service"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -11,8 +16,21 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(),
+		middlewares.BasicAuth(), gindump.Dump())
+
+	server.Use()
 
 	server.GET("/videos", func(c *gin.Context) {
 		c.JSON(200, videoController.FindAll())
