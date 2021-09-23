0323b290c8c0282ed854c718a62af24a01c99957
nictes1 <nikolastesone@gmail.com>
2021-09-23 12:18:30 -0300

    added save an findAll methods

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/nictes1/API-Rest-Golang/controller"
+	"github.com/nictes1/API-Rest-Golang/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
 )
 
 func main() {
-	r := gin.Default()
+	server := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
+	server.GET("/videos", func(c *gin.Context) {
+		c.JSON(200, videoController.FindAll())
 	})
-
-	r.Run("")
+	server.POST("/videos", func(c *gin.Context) {
+		c.JSON(200, videoController.Save(c))
+	})
+	server.Run(":8080")
 }
